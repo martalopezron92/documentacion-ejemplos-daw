3bf1eafa3f03d382d361cd27d5f708897abf7fda
Marta <mlopron829@g.educaand.es>
2025-11-10 16:58:36 +0100

    Deployed  with MkDocs version: 1.6.1

--- api/type-search-index.js
+++ api/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CalculadoraBuena"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"src","l":"CalculadoraBuena"}];updateSearchResults();
